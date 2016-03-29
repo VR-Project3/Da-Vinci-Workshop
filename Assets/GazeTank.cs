1a574a70ec2d12b59619ac241190701bcdf18c06
Erian Vazquez <erian.vazquez@gmail.com>
2016-03-28 21:30:25 -0400

    started impleting AR in VR with tank is another scene

---
+++ Assets/GazeTank.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GazeTank : MonoBehaviour {
+
+	bool isAnimating;
+
+//	public Transform projectile;
+//	public float bulletSpeed = 20;
+
+	SplineController tankSpline;
+
+	// Use this for initialization
+	void Start () {
+		isAnimating = false;
+	}
+
+	// Update is called once per frame
+	public void stareAtTankPaper() {
+		print ("hey");
+		foreach(Transform t in transform)
+		{   
+			if(t.name == "Tank"){
+				isAnimating = true;
+				t.gameObject.active = true;
+				tankSpline = t.GetComponent<SplineController>();
+				tankSpline.FollowSpline ();
+			
+			}
+			if(t.name == "Bullet"){
+				t.gameObject.active = true;
+				tankSpline = t.GetComponent<SplineController>();
+				tankSpline.FollowSpline ();
+
+			}
+		}
+	}
+}
