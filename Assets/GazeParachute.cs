e98092b1e927f31130f6768b7e05ecf11d34394a
Erian Vazquez <erian.vazquez@gmail.com>
2016-03-30 18:52:02 -0400

    added tank and para workshop

---
+++ Assets/GazeParachute.cs
@@ -0,0 +1,48 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GazeParachute : MonoBehaviour {
+
+	bool isAnimating;
+
+	SplineController tankSpline;
+	public GameObject Parachute;
+	private bool didRecordTimes = false;
+	private float startTime;
+
+	// Use this for initialization
+	void Start () {
+		isAnimating = false;
+	}
+
+	void Update(){
+
+		if(isAnimating){
+
+			if((startTime + 11.0f) < Time.time){
+				foreach (Renderer r in Parachute.GetComponentsInChildren<Renderer>()){
+					r.enabled = false;
+				}
+			}
+
+			if((startTime + 14.0f) < Time.time){
+				isAnimating = false;
+			}
+		}
+	}
+
+	// Update is called once per frame
+	public void stareAtParachutePaper() {
+
+		print ("test");
+		if(!isAnimating){
+			isAnimating = true;
+			foreach (Renderer r in Parachute.GetComponentsInChildren<Renderer>()){
+				r.enabled = true;
+			}
+			Parachute.GetComponent<SplineController>().FollowSpline ();
+
+			startTime = Time.time;
+		}
+	}
+}
